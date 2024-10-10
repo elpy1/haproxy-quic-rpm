3558d8c0a74a85adfa282fbd8369322de6aeae96
elpy <elpy@pm.me>
2024-10-10 14:02:04 +1100

    clean up fetch sources script

--- scripts/fetch_sources.sh
+++ scripts/fetch_sources.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit -o nounset
 
+AWS_LC_VERSION=${AWS_LC_VERSION:?AWS_LC_VERSION is not set}
+HAPROXY_VERSION=${HAPROXY_VERSION:?HAPROXY_VERSION is not set}
 SOURCES_DIR=${SOURCES_DIR:-.}
 
 download_aws_lc () {
@@ -16,24 +18,6 @@ download_haproxy () {
       mkdir -p "${SOURCES_DIR}"
       curl -s -L -o "${SOURCES_DIR}/haproxy-${HAPROXY_VERSION}.tar.gz" \
         "https://www.haproxy.org/download/${HAPROXY_VERSION%.*}/src/haproxy-${HAPROXY_VERSION}.tar.gz"
-  fi
-}
-
-# don't need on el9
-download_lua () {
-  if [ ! -f "${SOURCES_DIR}/lua-${LUA_VERSION}.tar.gz" ]; then
-      mkdir -p "${SOURCES_DIR}"
-      curl -s -L -o "${SOURCES_DIR}/lua-${LUA_VERSION}.tar.gz" \
-        "https://www.lua.org/ftp/lua-${LUA_VERSION}.tar.gz"
-  fi
-}
-
-# don't need on el9
-download_pcre2 () {
-  if [ ! -f "${SOURCES_DIR}/pcre2-${PCRE2_VERSION}.tar.gz" ]; then
-      mkdir -p "${SOURCES_DIR}"
-      curl -s -L -o "${SOURCES_DIR}/pcre2-${PCRE2_VERSION}.tar.gz" \
-        "https://github.com/PCRE2Project/pcre2/archive/refs/tags/pcre2-${PCRE2_VERSION}.tar.gz"
   fi
 }
 
